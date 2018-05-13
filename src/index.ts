d7575fa1497ade19291574ccf36f8babb297559e
alexknipfer <alexanderknipfer@gmail.com>
2018-05-13 17:42:48 -0500

    Add types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import * as Alexa from 'alexa-sdk'
 
 let handlers: Alexa.Handlers<Alexa.IntentRequest> = {
-  AboutIntent: () => {
+  AboutIntent: function() {
     let self: Alexa.Handler<Alexa.IntentRequest> = this
     let speechOutput = 'This skill was written by Alex Knipfer'
     self.emit(':tellWithCard', speechOutput, "Alex's skill", speechOutput)
   }
 }
 
-class Handler {
+export class Handler {
   constructor(
     event: Alexa.RequestBody<Alexa.Request>,
     context: Alexa.Context,
@@ -21,5 +21,8 @@ class Handler {
   }
 }
 
-export default (event, context, callback) =>
-  new Handler(event, context, callback)
+export default (
+  event: Alexa.RequestBody<Alexa.Request>,
+  context: Alexa.Context,
+  callback: Function
+) => new Handler(event, context, callback)
