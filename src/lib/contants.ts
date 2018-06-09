1d50822311b80a264513bfbd0a2655349c36c03f
alexknipfer <alexanderknipfer@gmail.com>
2018-06-08 21:12:30 -0400

    Add contants

---
+++ src/lib/contants.ts
@@ -0,0 +1,15 @@
+export enum RequestTypes {
+  Launch = 'LaunchRequest',
+  Intent = 'IntentRequest',
+  SessionEnded = 'SessionEndedRequest'
+}
+
+export enum IntentTypes {
+  Help = 'AMAZON.HelpIntent',
+  Stop = 'AMAZON.StopIntent',
+  Cancel = 'AMAZON.CancelIntent',
+  Fallback = 'AMAZON.FallbackIntent',
+  TopSpeakers = 'TopSpeakers',
+  UpcomingEvents = 'UpcomingEvents',
+  About = 'AboutIntent'
+}
