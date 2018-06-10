51854245816e409d9dd093dce53181672f16bf8c
alexknipfer <alexanderknipfer@gmail.com>
2018-06-09 19:23:44 -0500

    Add axios client

---
+++ src/services/AxiosClientImpl.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export class AxiosClientImpl {
+  public post(config: AxiosRequestConfig): Promise<AxiosResponse> {
+    return this.doRequest('post', config)
+  }
+
+  private doRequest(
+    method: string,
+    config: AxiosRequestConfig
+  ): Promise<AxiosResponse> {
+    config = { ...config, method }
+
+    try {
+      return axios.request(config)
+    } catch (err) {
+      console.log('ERROR HAS OCCURED: ', err)
+      throw new Error(err)
+    }
+  }
+}
