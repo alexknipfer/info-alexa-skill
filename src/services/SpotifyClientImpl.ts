b75095c1d47fbd0b8b7bc83a70f5fdb7ebb6c664
alexknipfer <alexanderknipfer@gmail.com>
2018-06-09 20:06:16 -0500

    Add getAccessToken

--- src/services/SpotifyClientImpl.ts
+++ src/services/SpotifyClientImpl.ts
@@ -1,12 +1,54 @@
 import { AxiosRequestConfig } from 'axios'
 import { encodeBase64 } from '../utils/StringUtils'
+import { AxiosClientImpl } from './AxiosClientImpl'
 
 export class SpotifyClientImpl {
-  private static readonly tokenUrl = '/api/token'
+  private axiosClient: any
+  private static readonly baseUrl = 'https://accounts.spotify.com/api/'
+  private static readonly tokenUrl = '/token'
 
-  public getAccessToken() {
+  constructor() {
+    this.axiosClient = new AxiosClientImpl()
+  }
+
+  public async getAccessToken() {
     const clientId = process.env.SPOTIFY_CLIENT_ID
     const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
     const base64auth = encodeBase64(`${clientId}:${clientSecret}`)
+
+    const headers = {
+      Authorization: `Basic ${base64auth}`
+    }
+
+    const config: AxiosRequestConfig = this.getConfig(
+      SpotifyClientImpl.tokenUrl,
+      headers,
+      '&grant_type=client_credentials'
+    )
+
+    try {
+      const { data } = await this.axiosClient.post(config)
+      console.log('RESULT: ', data)
+    } catch (error) {
+      console.log('THERE WAS AN ERROR: ', error)
+    }
+  }
+
+  private getConfig(
+    url: string,
+    headers?: {},
+    data?: {} | string
+  ): AxiosRequestConfig {
+    const config: AxiosRequestConfig = {
+      baseURL: SpotifyClientImpl.baseUrl,
+      url,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        ...headers
+      },
+      data: data ? data : {}
+    }
+
+    return config
   }
 }
