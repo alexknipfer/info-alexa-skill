5772a7b436293a75555138ed7536b58f82dd7a11
alexknipfer <alexanderknipfer@gmail.com>
2018-06-17 20:21:58 -0500

    Add fetchAccessToken

--- src/services/SpotifyClientImpl.ts
+++ src/services/SpotifyClientImpl.ts
@@ -4,10 +4,12 @@ import { encodeBase64 } from '../utils/StringUtils'
 import { AxiosClient } from './AxiosClient/AxiosClient'
 import { EnvConfig } from '../config/EnvConfig'
 import { InversifyTypes } from '../inversify.config'
+import { DynamoDBClient } from './DynamoDBClient/DynamoDBClient'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
 interface AccessTokenDetails {
   accessToken: string
-  expiresIn: string
+  expiresIn: number
   issuedAt: number
 }
 
@@ -17,10 +19,62 @@ export class SpotifyClientImpl {
 
   constructor(
     @inject(InversifyTypes.EnvConfig) private envConfig: EnvConfig,
-    @inject(InversifyTypes.AxiosClient) private axiosClient: AxiosClient
+    @inject(InversifyTypes.AxiosClient) private axiosClient: AxiosClient,
+    @inject(InversifyTypes.DynamoDBClient) private dynamoClient: DynamoDBClient
   ) {}
 
-  public async getAccessToken(): Promise<AccessTokenDetails> {
+  public async fetchAccessToken() {
+    const params: DocumentClient.GetItemInput = {
+      TableName: this.envConfig.spotifyTableName,
+      Key: {
+        id: 'AccessToken'
+      }
+    }
+
+    const result = await this.dynamoClient.get(params)
+    if (result && result.Item) {
+      const { expiresIn, issuedAt } = result.Item as AccessTokenDetails
+
+      const now = new Date().getTime()
+      const secondsElapsed = (now - issuedAt) / 1000
+
+      if (secondsElapsed > expiresIn) {
+        const accessTokenDetails = await this.getNewAccessToken()
+
+        const params: DocumentClient.PutItemInput = {
+          TableName: this.envConfig.spotifyTableName,
+          Item: { id: 'AccessToken', ...accessTokenDetails }
+        }
+
+        try {
+          await this.dynamoClient.put(params)
+        } catch (error) {
+          throw error
+        }
+
+        return accessTokenDetails
+      } else {
+        return result.Item
+      }
+    } else {
+      const accessTokenDetails = await this.getNewAccessToken()
+
+      const params: DocumentClient.PutItemInput = {
+        TableName: this.envConfig.spotifyTableName,
+        Item: { id: 'AccessToken', ...accessTokenDetails }
+      }
+
+      try {
+        await this.dynamoClient.put(params)
+      } catch (error) {
+        throw error
+      }
+
+      return accessTokenDetails
+    }
+  }
+
+  private async getNewAccessToken(): Promise<AccessTokenDetails> {
     const clientId = this.envConfig.spotifyClientId
     const clientSecret = this.envConfig.spotifyClientSecret
     const base64auth = encodeBase64(`${clientId}:${clientSecret}`)
