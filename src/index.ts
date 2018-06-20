6098d6c1d6dc9974111c9778de03f391d3836c97
alexknipfer <alexanderknipfer@gmail.com>
2018-06-19 21:07:01 -0500

    Add artist handler to index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import * as Alexa from 'ask-sdk'
 import * as dotenv from 'dotenv'
 import './InversifyBinder'
+import { InversifyConfiguration, InversifyTypes } from './inversify.config'
 import { AboutHandler } from './handlers/AboutHandler'
 import { LaunchHandler } from './handlers/LaunchHandler'
+import { ArtistTopTrackHandler } from './handlers/ArtistTopTrackHandler'
 
 dotenv.config()
 
 export const handler = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(new AboutHandler(), new LaunchHandler())
+  .addRequestHandlers(
+    InversifyConfiguration.container.get<AboutHandler>(
+      InversifyTypes.AboutHandler
+    ),
+    InversifyConfiguration.container.get<LaunchHandler>(
+      InversifyTypes.LaunchHandler
+    ),
+    InversifyConfiguration.container.get<ArtistTopTrackHandler>(
+      InversifyTypes.ArtistTopTrackHandler
+    )
+  )
   .lambda()
