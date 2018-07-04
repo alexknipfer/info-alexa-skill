84603355d4988d64a7c73ae71994915311896b0d
alexknipfer <alexanderknipfer@gmail.com>
2018-07-03 22:46:21 -0500

    Add expired token test

--- test/services/SpotifyClient.test.ts
+++ test/services/SpotifyClient.test.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment'
 import { EnvConfig } from '../../src/config/EnvConfig'
 import { SpotifyClient } from '../../src/services/SpotifyClient/SpotifyClient'
 import { SpotifyClientImpl } from '../../src/services/SpotifyClient/SpotifyClientImpl'
@@ -17,6 +18,8 @@ describe('SpotifyClient', () => {
   const DynamoDBClientMock = jest.fn<DynamoDBClient>()
   const EnvConfigMock = jest.fn<EnvConfig>(() => ({
     spotifyTableName: 'spotifyTable'
+    spotifyClientId: 'spotifyClientId',
+    spotifyClientSecret: 'spotifyClientSecret',
   }))
 
   beforeEach(() => {
@@ -79,6 +82,74 @@ describe('SpotifyClient', () => {
       .then((result: SpotifyArtist) => {
         expect(dynamoDBClientMock.get).toBeCalledWith(expectedDbParams)
         expect(axiosClientMock.get).toBeCalledWith(expectedAxiosConfig)
+        expect(result).toEqual(artistResult)
+      })
+  })
+
+  it('getArtistByName - successfully gets artist - expired access token', () => {
+    const accessTokenResult: DocumentClient.GetItemOutput = {
+      Item: {
+        accessToken: 'accessToken',
+        expiresIn: 3600,
+        issuedAt: moment().subtract(2, 'hours')
+      }
+    }
+
+    const newAccessTokenResult = {
+      data: {
+        access_token: 'newAccessToken',
+        expires_in: 3600,
+        issuedAt: Date.now(),
+      }
+    }
+
+    const tokenAxiosConfig: AxiosRequestConfig = {
+      baseURL: 'https://accounts.spotify.com/api/',
+      url: '/token',
+      headers: { 
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic c3BvdGlmeUNsaWVudElkOnNwb3RpZnlDbGllbnRTZWNyZXQ='
+      },
+      data: '&grant_type=client_credentials'
+    }
+
+    const artistAxiosConfig: AxiosRequestConfig = {
+      baseURL: 'https://api.spotify.com/v1/',
+      url: '/search?q=drake&type=artist',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${newAccessTokenResult.data.access_token}`
+      },
+      data: undefined
+    }
+
+    const artistResult = getSpotifyArtist()
+
+    dynamoDBClientMock.get = jest.fn(
+      () => new Promise(resolve => resolve(accessTokenResult))
+    )
+    dynamoDBClientMock.put = jest.fn(
+      () => new Promise(resolve => resolve())
+    )
+    axiosClientMock.post = jest.fn(() => new Promise(resolve => resolve(newAccessTokenResult)))
+    axiosClientMock.get = jest.fn(
+      () =>
+        new Promise(resolve =>
+          resolve({
+            data: {
+              artists: {
+                items: [artistResult]
+              }
+            }
+          })
+        )
+    )
+
+    return spotifyClient
+      .getArtistByName('drake')
+      .then((result: SpotifyArtist) => {
+        expect(axiosClientMock.post).toBeCalledWith(tokenAxiosConfig)
+        expect(axiosClientMock.get).toBeCalledWith(artistAxiosConfig)
         expect(result).toEqual(artistResult)
       })
   })
